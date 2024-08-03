9e80c1d76c517bd990d6baef622ee95f7672332d
Andrei-Biareshchanka <a.biareshchanka@gmail.com>
2024-08-03 18:19:48 +0200

    feat: update configs for work with svg

---
+++ next.config.js
@@ -0,0 +1,17 @@
+import withPlugins from 'next-compose-plugins';
+import svgr from '@svgr/webpack';
+
+const nextConfig = {
+  reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+};
+
+export default withPlugins([], nextConfig);
