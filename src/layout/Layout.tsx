83cb7c253613def491b3920c7e3824f868a03275
Andrei-Biareshchanka <a.biareshchanka@gmail.com>
2024-08-09 13:23:45 +0200

    feat: update Layout compoent and styles

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,4 +1,3 @@
-import cn from 'classnames';
 import { LayoutProps } from './LayoutProps';
 import styles from './Layout.module.css';
 import { Header } from './Header/Header';
@@ -8,14 +7,12 @@ import { FunctionComponent } from 'react';
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
   return (
-    <>
-      <Header />
-      <div>
-        <Sidebar />
-        <div>{children}</div>
-      </div>
-      <Footer />
-    </>
+    <div className={styles.wrapper}>
+      <Header className={styles.header} />
+      <Sidebar className={styles.sidebar} />
+      <div className={styles.body}>{children}</div>
+      <Footer className={styles.footer} />
+    </div>
   );
 };
 
