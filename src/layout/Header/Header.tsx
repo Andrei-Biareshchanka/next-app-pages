67a29c5a008e3307d8f6fb466c8a44c68797135d
Andrei-Biareshchanka <a.biareshchanka@gmail.com>
2024-08-11 13:28:07 +0200

    fix: remove import

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,5 +1,4 @@
 import { HeaderProps } from './HeaderProps';
-import styles from './Sidebar.module.css';
 
 export const Header = ({ ...props }: HeaderProps): JSX.Element => {
   return <div {...props}>Header</div>;
