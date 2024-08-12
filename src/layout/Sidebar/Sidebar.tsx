d86a755a79d2bfea4913ce985d5b95d26cea412f
Andrei-Biareshchanka <a.biareshchanka@gmail.com>
2024-08-12 16:42:44 +0200

    feat: update Sidebar component and styles

--- src/layout/Sidebar/Sidebar.tsx
+++ src/layout/Sidebar/Sidebar.tsx
@@ -1,9 +1,16 @@
 import { Menu } from '../Menu/Menu';
 import { SidebarProps } from './SidebarProps';
+import cn from 'classnames';
+import styles from './Sidebar.module.css';
+import Logo from '../logo.svg';
 
-export const Sidebar = ({ ...props }: SidebarProps): JSX.Element => {
+export const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
   return (
-    <div {...props}>
+    <div className={cn(className, styles.sidebar)} {...props}>
+      <div className={styles.logo}>
+        <Logo />
+      </div>
+      <div>Поиск</div>
       <Menu />
     </div>
   );
