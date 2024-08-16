c8a039c3a3d9b11b5c6d3520b65ef03be079d8e0
Andrei-Biareshchanka <a.biareshchanka@gmail.com>
2024-08-16 14:31:21 +0200

    feat: update nextConfig with paths for images

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ import withPlugins from 'next-compose-plugins';
 import svgr from '@svgr/webpack';
 
 const nextConfig = {
+  images: {
+		domains: ['courses-top.ru', 'old-images.hb.ru-msk.vkcs.cloud']
+	},
   reactStrictMode: true,
   webpack(config) {
     config.module.rules.push({
