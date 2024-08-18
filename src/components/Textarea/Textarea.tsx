35ef605de8105b1aec4847e4531949481a87f26e
Andrei-Biareshchanka <a.biareshchanka@gmail.com>
2024-08-18 14:57:36 +0200

    feat: update Textarea component with styles and props

--- src/components/Textarea/Textarea.tsx
+++ src/components/Textarea/Textarea.tsx
@@ -4,11 +4,20 @@ import styles from './Textarea.module.css';
 import { ForwardedRef, forwardRef } from 'react';
 
 export const TextareaRef = (
-  { className, ...props }: TextareaProps,
+  { className, error, ...props }: TextareaProps,
   ref: ForwardedRef<HTMLTextAreaElement>
 ): JSX.Element => {
   return (
-    <textarea ref={ref} className={cn(className, styles.textarea)} {...props} />
+    <div className={cn(styles.textareaWrapper, className)}>
+      <textarea
+        ref={ref}
+        className={cn(styles.textarea, {
+          [styles.error]: error,
+        })}
+        {...props}
+      />
+      {error && <span className={styles.errorMessage}>{error.message}</span>}
+    </div>
   );
 };
 
